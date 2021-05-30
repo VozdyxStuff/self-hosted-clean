b3863016624255e37e20adbfc14c43e5b1a0679a
Vozdyx <pasha.igin.v@gmail.com>
2021-05-30 13:21:47 +0300

    Update cleanup.js

--- cleanup.js
+++ cleanup.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
 
-exec('cd ../../ && rm -Rf $REPO_NAME _PipelineMapping _temp _tool',
+exec('cd ../../ && rm -Rf $REPO_NAME _PipelineMapping _temp _tool _actions',
     function (error, stdout, stderr) {
         console.log('stdout: ' + stdout);
         console.log('stderr: ' + stderr);
