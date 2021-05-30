589e4cdc23ed2dff9ae71b24629f0bee5dec23da
Vozdyx <pasha.igin.v@gmail.com>
2021-05-30 13:13:45 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-var exec = require('child_process').exec, child;
 
-child = exec('cd ../../ && rm -Rf $REPO_NAME _PipelineMapping _temp _tool',
-    function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-             console.log('exec error: ' + error);
-        }
-    });
- child();
